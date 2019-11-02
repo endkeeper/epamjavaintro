6d3452999fac4c59331d06c68db1baa3eba68f76
Denis Merechko <denismerechko99@gmail.com>
2019-11-02 18:23:48 +0300

    add second task

---
+++ 1_Basics_of_software_code_development/branching/Task2.java
@@ -0,0 +1,35 @@
+/**
+ * Найти max{min(a,b), min(c,d)}
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+        result(4, 3, 1, 4);
+    }
+
+    static void result(int a, int b, int c, int d) {
+        int min1;
+        int min2;
+        int max;
+
+        if (a > b) {
+            min1 = b;
+        } else {
+            min1 = a;
+        }
+
+        if (c > d) {
+            min2 = d;
+        } else {
+            min2 = c;
+        }
+
+        if (min2 > min1) {
+            max = min2;
+        } else {
+            max = min1;
+        }
+
+        System.out.println("result is " + max);
+    }
+}
