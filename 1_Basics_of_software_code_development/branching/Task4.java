6749bb69a87a2d60c6fa47b9b2fa8077000f4a45
Denis Merechko <denismerechko99@gmail.com>
2019-11-02 23:40:35 +0300

    add fourth task

---
+++ 1_Basics_of_software_code_development/branching/Task4.java
@@ -0,0 +1,22 @@
+/**
+ * Заданы размеры отверстия a,b и размеры кирпича x,y,z. Определить, пройдет ли кирпич в
+ * отверстие
+ */
+
+public class Task4 {
+    public static void main(String[] args) {
+        brick(5, 2, 1, 2, 3);
+    }
+
+    static void brick(int a, int b, int x, int y, int z) {
+        boolean pos1 = a > x && b > y;
+        boolean pos2 = a > z && b > y;
+        boolean pos3 = a > y && b > x;
+
+        if (pos1 || pos2 || pos3) {
+            System.out.println("Brick can go through hole");
+        } else {
+            System.out.println("Brick cant go through hole");
+        }
+    }
+}
