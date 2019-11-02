0746afaacadd6e56fc02677da2238a12cad00d29
Denis Merechko <denismerechko99@gmail.com>
2019-11-02 19:53:27 +0300

    add third task

---
+++ 1_Basics_of_software_code_development/branching/Task3.java
@@ -0,0 +1,19 @@
+/**
+ * Заданы три точки. Определить, расположены ли они на одной прямой
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+        points(1, 2, 1, 3, 1, 5);
+    }
+
+    static void points(int x1, int y1, int x2, int y2, int x3, int y3) {
+        int statement1 = (x1 - x2) * (y3 - y2);
+        int statement2 = (x3 - x2) * (y1 - y2);
+        if (statement1 == statement2) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
