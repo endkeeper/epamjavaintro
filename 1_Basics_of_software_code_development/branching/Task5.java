c84973d7d1ffb75d185c43aa8230e9efc8250e2a
Denis Merechko <denismerechko99@gmail.com>
2019-11-03 14:02:25 +0300

    add fifth task

---
+++ 1_Basics_of_software_code_development/branching/Task5.java
@@ -0,0 +1,19 @@
+/**
+ * Вычислить значение функции
+ */
+
+public class Task5 {
+    public static void main(String[] args) {
+        function(1);
+    }
+
+    static void function(int x) {
+        double result;
+        if (x <= 3) {
+            result = Math.pow(x, 2) - 3 * x + 9;
+        } else {
+            result = 1 / (Math.pow(x, 3) + 6);
+        }
+        System.out.println("result is " + result);
+    }
+}
