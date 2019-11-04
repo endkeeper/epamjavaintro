b1ded259353a50626fb901da53cec09910a6264b
Denis Merechko <denismerechko99@gmail.com>
2019-11-04 20:51:56 +0300

    add sixth, seventh and eights tasks

--- 1_Basics_of_software_code_development/cycles/Task6.java
+++ 1_Basics_of_software_code_development/cycles/Task6.java
@@ -1,8 +1,22 @@
 package cycles;
 
 /**
- *
+ * Вывести на экран соответствия между символами и их численными обозначениями в памяти компьютера
  */
 
 public class Task6 {
+    public static void main(String[] args) {
+        symbols("13123");
+        symbols("sadasdqw");
+        symbols("$#@^@#esd312");
+    }
+
+    static void symbols(String x) {
+        char[] symbols = x.toCharArray();
+        for (int i = 0; i < symbols.length; i++) {
+            int result = (int) symbols[i];
+            System.out.println(symbols[i] + " in ASCII is " + result);
+        }
+        System.out.println();
+    }
 }
