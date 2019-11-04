c19df7cf90d0e9d32614d50b3766e90ad5cdb7a4
Denis Merechko <denismerechko99@gmail.com>
2019-11-04 16:46:08 +0300

    add eighth task

--- 1_Basics_of_software_code_development/cycles/Task8.java
+++ 1_Basics_of_software_code_development/cycles/Task8.java
@@ -1,9 +1,26 @@
+/**
+ * Даны два числа. Определить цифры, входящие в запись как первого так и второго числа
+ */
+
 public class Task8 {
-    public static void main(String[] args){
-        numbers(1,2);
+    public static void main(String[] args) {
+        numbers(10, 2);
+        numbers(-10, 4);
+        numbers(15432, -41232);
+        numbers(5342, 0);
     }
 
-    static void numbers(int x, int y){
-        System.out.println(x/y);
+    static void numbers(int x, int y) {
+        String firstNumber = "" + x;
+        String secondNumber = "" + y;
+
+        for (int i = 1; i <= firstNumber.length(); i++) {
+            System.out.println(i + " number of first figure: " + firstNumber.charAt(firstNumber.length() - i));
+        }
+        System.out.println();
+        for (int i = 1; i <= secondNumber.length(); i++) {
+            System.out.println(i + " number of second figure: " + secondNumber.charAt(secondNumber.length() - i));
+        }
+
     }
 }
