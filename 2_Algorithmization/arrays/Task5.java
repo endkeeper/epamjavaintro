1a1dc6069aec11482f97c053f52c8712522071a7
Denis Merechko <denismerechko99@gmail.com>
2019-12-10 01:18:13 +0300

    add 5 task

---
+++ 2_Algorithmization/arrays/Task5.java
@@ -0,0 +1,20 @@
+package arrays;
+
+/*
+Даны целые числа a1, a2,..., an. Вывести на печать только те числа, для которых ai>i.
+ */
+
+public class Task5 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{4, 1, 6, 3, 2, 1};
+        numbers(arr);
+    }
+
+    static void numbers(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > i) {
+                System.out.println(arr[i] + " ");
+            }
+        }
+    }
+}
