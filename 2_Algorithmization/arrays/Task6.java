2fea6eec36b813afdc5f45f271a9951564f3d37e
Denis Merechko <denismerechko99@gmail.com>
2019-12-10 21:15:35 +0300

    add 6 task

---
+++ 2_Algorithmization/arrays/Task6.java
@@ -0,0 +1,30 @@
+package arrays;
+
+/*
+Задана последовательность N вещественных чисел. Вычислить сумму чисел, порядкове номера которых являются простыми числами
+ */
+
+public class Task6 {
+    public static void main(String[] args) {
+        float[] arr = new float[]{2f, 1f, 21f, 5f, 3f, 1f, 2f, 5f, 6f};
+        sum(arr);
+    }
+
+    static void sum(float[] x) {
+        float sum = x[2];
+        boolean check = true;
+        for (int i = 3; i < x.length; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check == true) {
+                sum += x[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
