dd5bd7c9a8ac658a36fa56aae130c63ddd9e4a50
Denis Merechko <denismerechko99@gmail.com>
2019-12-18 21:52:03 +0300

    Add and edit tasks

--- 2_Algorithmization/arrays_of_arrays/Task12.java
+++ 2_Algorithmization/arrays_of_arrays/Task12.java
@@ -4,9 +4,12 @@ package arrays_of_arrays;
 Отсортировать строки матрицы по возрастанию и убыванию значений элементов*
  */
 
+import java.util.Arrays;
+
 public class Task12 {
     public static void main(String[] args) {
         matrix(5);
+        matrix(7);
     }
 
     private static void matrix(int n) {
@@ -20,15 +23,17 @@ public class Task12 {
             System.out.println();
         }
         System.out.println();
-        int max;
-        for (int row = 0; row < n; row++) {
-            for (int column = 0; column < n; column++) {
-
-                System.out.print(arr[row][column] + " ");
+        for (int[] x : arr) {
+            Arrays.sort(x);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
 }
+
 
 
