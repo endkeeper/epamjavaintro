a8ee6be3e4600a63dab10e0b60c3fcb78760f916
mentoracc <57094557+mentoracc@users.noreply.github.com>
2019-12-28 20:45:13 +0300

    Update Ex04.java

--- 2_Algorithmization/example/Ex04.java
+++ 2_Algorithmization/example/Ex04.java
@@ -3,15 +3,15 @@ package example;
 public class Ex04 {
     public static void main(String[] args) {
         // обращение к элементам массива
-        int[] a = {1, 2, 3};
-        int[] b = new int[49];
+        int[] a = {1, 2, 3};    
         byte i = 1;
         short i1 = 2;
         int i3 = 2;
         System.out.println(a[i]);
         System.out.println(a[i1]);
         System.out.println(a[i3]);
-
+        
+        int[] b = new int[49];
         char i4 = '0';
         b[i4] = 5;
         System.out.println(b[i4]);
